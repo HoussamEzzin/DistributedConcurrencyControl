7359a5bbb89f2799b09226fe9a64fef06742270d
Ezzin Houssam <ezzinhoussam@gmail.com>
2021-12-28 22:34:37 +0100

    initial

---
+++ bto_tm.py
@@ -0,0 +1,23 @@
+
+def bto_tm(msg):
+	while(msg):
+		if msg.type == 'transaction operation':
+			#let op be the operation
+			if op.Type == 'BT':
+				S = None
+				#assign timestamp ts(T) to transaction
+				dp(op)
+			elif op.Type == 'R' or op.Type == 'W':
+				print('Say Si')
+				bto_sc(op,ts(T))#Si
+				S = S + Si
+			elif op.Type == 'A' or op.Type == 'C':
+				dp(op)#S
+		elif msg.type = 'SC response':
+			op.Type = A 
+			bto_sc(op,_)#S
+			#restart transaction with a new timestamp
+		elif msg.type == 'DP response':
+			#let op be the operation
+			if transaction_op_type == 'R':
+				return op.value
---
+++ c2pl.py
@@ -0,0 +1,66 @@
+
+
+def c2pl_tm(msg):
+	while(msg):
+		if msg == 'transaction operation':
+			#let op be the operation
+			if op.type == BT:
+				dp(op)
+			else:
+				c2pl_lm(op)
+		elif msg == 'Lock Manager response':
+			if lock_request_granted == True:
+				print('say Hi')
+				dp(op)#Si
+			else:
+				print('Transaction terminated')
+		elif msg == 'Data Processor response':
+			if to == 'R':
+				return op.value
+			elif to == 'W':
+				print('Completion of the write')
+			elif to == 'C':
+				if commit_msg_recieved == True:
+					print('Successful completion of transaction')
+					c2pl_lm(op)
+				else:
+					print('arrival of the commit message recorded')
+			elif to == 'A':
+					print('Completion of the abort')
+					c2pl_lm(op)
+
+
+def c2pl_lm(op):
+	if op.Type == 'R' or op.Type == 'W':
+		#fin the lock nit  lu such that op.arg belongs to lu
+		if lu_unlocked == True or lock_mode == op.Type :
+			#set lock on lu in appropriate mode on behalf of transaction op.tid
+			print("Lock granted")
+		else:
+			print("op on queue for lu")
+	elif op.Type == 'C' or op.Type == 'A' :
+		for lu in lu_held_by_transaction :
+			#release lock on lu held by transaction
+			if operations in operation_waiting_for_queue:
+				#find the first operation O on queue
+				#set a lock on lu on behalf of O
+				print("Lock granted")
+		print("Locks released")
+
+def dp(op):
+	if op.Type == 'BT':
+		print('bookkeeping')
+	elif op.Type == 'R':
+		op.res = READ(op.arg)
+		op.res = "Read done"
+	elif op.Type == 'W':
+		WRITE(op.arg,op.val)
+		op.res = "Write done"
+	elif op.Type == 'C':
+		COMMIT
+		op.res = "Commit done"
+	elif op.Type == 'A':
+		ABORT
+		op.res = "Abort done"
+
+	return op
